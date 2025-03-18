882afc92b002d365950f74e677b99068c924f562
Shivansh Vij <shivanshvij@loopholelabs.io>
2025-03-18 15:56:33 -0700

    fix: adding `kvm_mmu_unprotect_page_virt` to exported functions from MMU

--- arch/x86/kvm/mmu.h
+++ arch/x86/kvm/mmu.h
@@ -113,6 +113,7 @@ void kvm_init_shadow_ept_mmu(struct kvm_vcpu *vcpu, bool execonly,
 bool kvm_can_do_async_pf(struct kvm_vcpu *vcpu);
 int kvm_handle_page_fault(struct kvm_vcpu *vcpu, u64 error_code,
 				u64 fault_address, char *insn, int insn_len);
+int kvm_mmu_unprotect_page_virt(struct kvm_vcpu *vcpu, gva_t gva);
 void __kvm_mmu_refresh_passthrough_bits(struct kvm_vcpu *vcpu,
 					struct kvm_mmu *mmu);
 
