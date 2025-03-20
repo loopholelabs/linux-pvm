e47745fb1020845737db58122197cd79a956cd4b
Shivansh Vij <shivanshvij@outlook.com>
2025-03-19 20:57:36 -0700

    Revert "fix: make necessary functions exported"
    This reverts commit 528334185d746208089470a76127d37256a363e6.

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -2069,7 +2069,6 @@ void kvm_mmu_free_roots(struct kvm *kvm, struct kvm_mmu *mmu,
 bool kvm_mmu_slot_gfn_write_protect(struct kvm *kvm,
 		struct kvm_memory_slot *slot, u64 gfn,
 		int min_level);
-void kvm_mmu_gfn_disallow_lpage(const struct kvm_memory_slot *slot, gfn_t gfn);
 void kvm_mmu_free_guest_mode_roots(struct kvm *kvm, struct kvm_mmu *mmu);
 gpa_t kvm_mmu_gva_to_gpa_read(struct kvm_vcpu *vcpu, gva_t gva,
 			      struct x86_exception *exception);
