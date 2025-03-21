e5d1fdd5195901fa4ab22f2b8ded970ce1132a6b
Shivansh Vij <shivanshvij@outlook.com>
2025-03-20 20:12:03 -0700

    feat: userspace mmu

--- arch/x86/kernel/paravirt.c
+++ arch/x86/kernel/paravirt.c
@@ -260,6 +260,7 @@ struct paravirt_patch_template pv_ops = {
 
 	.mmu.set_pte		= native_set_pte,
 	.mmu.set_pmd		= native_set_pmd,
+	.mmu.set_pmd		= native_set_pmd,
 
 	.mmu.ptep_modify_prot_start	= __ptep_modify_prot_start,
 	.mmu.ptep_modify_prot_commit	= __ptep_modify_prot_commit,
